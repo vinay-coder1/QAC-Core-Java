33227bc3451b3ee54b6891cdfa452f5f6cf745e3
= <=>
2021-12-10 20:18:51 +0530

    Adding 2 files

---
+++ EmployeeLogin.java
@@ -0,0 +1,19 @@
+public class EmployeeLogin{
+
+public static void main(String[] args){
+       System.out.println("Main Method Started");
+
+       boolean isIdAvailable = true;
+       boolean boimetric = true;
+
+           
+       if(isIdAvailable && boimetric){ //true
+        System.out.println("Inside if block");
+        System.out.println("Employe Logined");  
+       }
+
+
+      System.out.println("Main Method Ended");  
+}   
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{  //class Indentifier
+
+//This is main method
+
+/*
+ main method is the starting point of
+ java program
+*/	
+public static void main(String[] args){
+
+     //statment to print meassage    
+     System.out.println("Main Method started");
+
+     System.out.println("Hello QAC People"); 
+
+     System.out.println("Main Method ended");    	
+   } 
+}
+
